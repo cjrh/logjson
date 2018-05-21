e82cf7af6ef63110ec54b077d20dade4e978cfe8
Caleb Hattingh <caleb.hattingh@gmail.com>
2018-05-21 21:52:22 +1000

    Use a formatter subclass, rather than a handler

--- logjson.py
+++ logjson.py
@@ -17,20 +17,21 @@ import copy
 __version__ = '2017.12.1'
 
 
-class JSONHandler(logging.StreamHandler):
+class JSONFormatter(logging.Formatter):
     def __init__(self, logstash_mode=False, pretty=False, *args, **kwargs):
-        # super().__init__(*args, stream=sys.stdout, **kwargs)
-        super(JSONHandler, self).__init__(*args, **kwargs)
+        super(JSONFormatter, self).__init__(*args, **kwargs)
         self.logstash_mode = logstash_mode
         self.pretty = pretty
 
     def format(self, record):
-        # type: (logging.LogRecord) -> str
-        """Don't even need a Formatter class at all."""
         record.message = record.getMessage()
+        if self.usesTime():  # pragma: no cover
+            record.asctime = self.formatTime(record, self.datefmt)
+
         if hasattr(record, 'exc_info') and record.exc_info:
             record.exc_text = ''.join(traceback.format_exception(*record.exc_info))
             record.message += '\n' + record.exc_text
+
         # Actual isoformat. self.formatTime() is not the same.
         if sys.version_info.major == 2:  # pragma: no cover
             record.created_iso = datetime.datetime.fromtimestamp(
